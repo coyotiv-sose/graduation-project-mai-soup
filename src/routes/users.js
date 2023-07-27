c72587932bef546a231c32904c5cc6f91b9ebb1b
Maijs Garais <97809168+mai-soup@users.noreply.github.com>
2023-07-27 22:25:13 +0000

    create route and view for single user, refactor views to be split into subfolders

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,13 +8,27 @@ router.get('/', (req, res, next) => {
     return res.send(User.list)
   }
 
-  return res.render('users', { users: User.list })
+  return res.render('users/users', { users: User.list })
 })
 
 router.post('/', (req, res) => {
   const { username, email } = req.body
   const user = User.createUser({ username, email })
   res.send(user)
+})
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+  const user = User.list.filter(u => u.username === id)[0]
+  if (!user) {
+    return res.status(404).send('User not found')
+  }
+
+  if (req.query.json) {
+    return res.send(user)
+  }
+
+  return res.render('users/user', { user })
 })
 
 module.exports = router
