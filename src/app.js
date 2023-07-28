8f2649ea01634621b37afbcb516528f91c0eca18
Maijs Garais <97809168+mai-soup@users.noreply.github.com>
2023-07-28 19:21:16 +0000

    add and integrate mongoose

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 require('dotenv').config()
+require('./database-connection')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
---
+++ src/database-connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+mongoose.connect(process.env.MONGO_URI)
+
+const Cat = mongoose.model('Cat', { name: String })
+
+const kitty = new Cat({ name: 'Zildjian' })
+kitty.save().then(() => console.log('meow'))
