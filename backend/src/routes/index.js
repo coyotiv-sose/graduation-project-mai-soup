b35aa65f7d1d7ce4050831742334955fa57ab9b8
Maijs Garais <97809168+mai-soup@users.noreply.github.com>
2023-08-08 17:08:04 +0000

    fix more linting

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const router = express.Router()
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res) => {
   res.render('index', { title: 'PageFlipper' })
 })
 
