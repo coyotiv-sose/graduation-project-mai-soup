8e20d89eb1698e7df1e7d05f9f02480f02d9ba33
Maijs Garais <97809168+mai-soup@users.noreply.github.com>
2023-08-08 20:22:59 +0000

    add cors middleware to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const cors = require('cors')
 require('./database-connection')
 
 const indexRouter = require('./routes/index')
@@ -11,6 +12,8 @@ const bookshelvesRouter = require('./routes/bookshelves')
 const booksRouter = require('./routes/books')
 
 const app = express()
+
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
