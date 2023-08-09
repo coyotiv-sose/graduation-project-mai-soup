44dfcc3967e063bfbaf359a27c30eea747f438f3
Maijs Garais <97809168+mai-soup@users.noreply.github.com>
2023-08-09 19:36:46 +0000

    fix using wrong user field for key in usersview

--- frontend/src/views/UsersView.vue
+++ frontend/src/views/UsersView.vue
@@ -1,16 +1,25 @@
-<script setup>
+<script>
 import axios from 'axios'
 
-// TODO: probably makes more sense to send users directly
-// instead of wrapping them in an object
-const { data: userData } = await axios.get('http://localhost:40927/users?json=true')
-const users = userData.users
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  async mounted() {
+    // TODO: probably makes more sense to send users directly
+    // instead of wrapping them in an object
+    const { data: userData } = await axios.get('http://localhost:40927/users?json=true')
+    this.users = userData.users
+  }
+}
 </script>
 
 <template lang="pug">
 main
   h1 Users
   ul
-    li(v-for='user in users', :key='user.id')
+    li(v-for='user in users', :key='user._id')
       | {{ user.username }}
 </template>
