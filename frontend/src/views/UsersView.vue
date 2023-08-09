6509903ef4938444bdf5db404568fa63fc0de172
Maijs Garais <97809168+mai-soup@users.noreply.github.com>
2023-08-09 19:23:18 +0000

    refactor users view to use pug

--- frontend/src/views/UsersView.vue
+++ frontend/src/views/UsersView.vue
@@ -3,17 +3,14 @@ import axios from 'axios'
 
 // TODO: probably makes more sense to send users directly
 // instead of wrapping them in an object
-const { data: userData } = await axios.get('http://localhost:3000/users?json=true')
+const { data: userData } = await axios.get('http://localhost:40927/users?json=true')
 const users = userData.users
 </script>
 
-<template>
-  <main>
-    <h1>Users</h1>
-    <ul>
-      <li v-for="user in users" :key="user.id">
-        {{ user.username }}
-      </li>
-    </ul>
-  </main>
+<template lang="pug">
+main
+  h1 Users
+  ul
+    li(v-for='user in users', :key='user.id')
+      | {{ user.username }}
 </template>
