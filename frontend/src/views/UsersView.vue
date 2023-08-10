9ad5fbaa373ebd292f3d9c1ec568ce4d093a2c0c
Maijs Garais <97809168+mai-soup@users.noreply.github.com>
2023-08-10 19:59:12 +0000

    add form to add a user

--- frontend/src/views/UsersView.vue
+++ frontend/src/views/UsersView.vue
@@ -4,7 +4,28 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      users: []
+      users: [],
+      username: '',
+      email: ''
+    }
+  },
+  methods: {
+    async submitForm() {
+      try {
+        const { data: userData } = await axios.post('http://localhost:3000/users', {
+          username: this.username,
+          email: this.email
+        })
+
+        // clear the input fields - DOES NOT WORK
+        this.username = ''
+        this.email = ''
+
+        this.users.push(userData)
+      } catch (error) {
+        // TODO: handle error
+        console.error('error creating user:', error)
+      }
     }
   },
   async mounted() {
@@ -19,6 +40,18 @@ export default {
 <template lang="pug">
 main
   h1 Users
+
+  h2 Register
+  form(@submit.prevent="submitForm")
+    div
+      label(for="username") Username:
+      input(type="text" id="username" v-model="username" required)
+    div
+      label(for="email") Email:
+      input(type="email" id="email" v-model="email" required)
+    button(type="submit") Submit
+
+  h2 Signed up users
   ul
     li(v-for='user in users', :key='user._id')
       | {{ user.username }}
