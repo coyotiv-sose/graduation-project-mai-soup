4f60ffaa9dc0959489efdce70d4b6a070e0434a7
Maijs Garais <97809168+mai-soup@users.noreply.github.com>
2023-08-10 20:15:13 +0000

    add view for single user

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import UsersView from '../views/UsersView.vue'
+import UserView from '../views/UserView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/users',
       name: 'users',
       component: UsersView
+    },
+    {
+      path: '/users/:username',
+      name: 'user',
+      component: UserView
     },
     {
       path: '/about',
---
+++ frontend/src/views/UserView.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      user: {}
+    }
+  },
+  async mounted() {
+    const { data: userData } = await axios.get(
+      `http://localhost:3000/users/${this.$route.params.username}`
+    )
+    this.user = userData
+
+    // TODO: handle error
+  }
+}
+</script>
+
+<template lang="pug">
+h1 {{ user.username }}
+ul
+  li Email: {{ user.email }}
+  li Created at: {{ user.dateCreated }}
+</template>
