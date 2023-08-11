581b7b6b7391c8b3adff029c5b1f6bfe98232b40
Maijs Garais <97809168+mai-soup@users.noreply.github.com>
2023-08-11 07:47:06 +0000

    handle errors gracefully in book routes

--- backend/src/routes/books.js
+++ backend/src/routes/books.js
@@ -5,22 +5,50 @@ const Book = require('../models/book')
 
 router.get('/:isbn', (req, res) => {
   const { isbn } = req.params
-  const book = Book.findOne({ isbn })
 
-  if (!book) {
-    return res.status(404).send('Book not found')
+  try {
+    const book = Book.findOne({ isbn })
+
+    if (!book) {
+      return res.status(404).send('Book not found')
+    }
+
+    return res.send(book)
+  } catch (err) {
+    console.error(err)
+    return res
+      .status(500)
+      .send(
+        'An error occurred while retrieving the book. Please try again later.'
+      )
   }
-
-  return res.send(book)
 })
 
 router.post('/', async (req, res) => {
   const { isbn, title, author } = req.body
-  const book = new Book({ isbn, title, author })
 
-  await book.save()
+  if (!isbn || !title || !author) {
+    return res.status(400).send('ISBN, title, and author are required')
+  }
 
-  return res.status(201).send(book)
+  try {
+    const book = await Book.create({ isbn, title, author })
+    return res.status(201).send(book)
+  } catch (err) {
+    // code 11000 represents a duplicate key error in mongo
+    if (error.code === 11000) {
+      return res.status(409).json({ error: 'ISBN already exists' })
+    }
+
+    // other errors
+    console.error(error)
+    res
+      .status(500)
+      .json({
+        error:
+          'An error occured while adding the book. Please try again later.',
+      })
+  }
 })
 
 module.exports = router
