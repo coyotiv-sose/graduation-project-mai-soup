7e022067c848cdc0c43a5563511872f8e69bdd94
Maijs Garais <97809168+mai-soup@users.noreply.github.com>
2023-08-11 07:52:15 +0000

    fix undefined variables and linting

--- backend/src/routes/books.js
+++ backend/src/routes/books.js
@@ -16,11 +16,7 @@ router.get('/:isbn', (req, res) => {
     return res.send(book)
   } catch (err) {
     console.error(err)
-    return res
-      .status(500)
-      .send(
-        'An error occurred while retrieving the book. Please try again later.'
-      )
+    return res.status(500).send('An error occurred while retrieving the book. Please try again later.')
   }
 })
 
@@ -36,18 +32,15 @@ router.post('/', async (req, res) => {
     return res.status(201).send(book)
   } catch (err) {
     // code 11000 represents a duplicate key error in mongo
-    if (error.code === 11000) {
+    if (err.code === 11000) {
       return res.status(409).json({ error: 'ISBN already exists' })
     }
 
     // other errors
-    console.error(error)
-    res
-      .status(500)
-      .json({
-        error:
-          'An error occured while adding the book. Please try again later.',
-      })
+    console.error(err)
+    return res.status(500).json({
+      error: 'An error occured while adding the book. Please try again later.',
+    })
   }
 })
 
