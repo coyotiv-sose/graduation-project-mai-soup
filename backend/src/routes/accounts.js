e591a1b03502840ae2b89072d617fd49834c32ac
Maijs Garais <97809168+mai-soup@users.noreply.github.com>
2023-08-14 20:20:52 +0000

    add route for registration and getting current session

---
+++ backend/src/routes/accounts.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+
+router.get('/', (req, res) => {
+  res.send(req.session)
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const { username, email, password } = req.body
+
+    const user = await User.register({ username, email }, password)
+
+    return res.send(user)
+  } catch (err) {
+    return res.status(400).send({ error: 'Registration failed' })
+  }
+})
+
+module.exports = router
