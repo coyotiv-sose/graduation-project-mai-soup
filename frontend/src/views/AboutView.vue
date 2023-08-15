2e20d3b5a8958dac6015c252a750e6a08dee2d0e
Maijs Garais <97809168+mai-soup@users.noreply.github.com>
2023-08-15 18:42:39 +0000

    remove old store functionality from usersview and aboutview

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,19 +1,11 @@
-<script>
-import { useAccountStore } from '../stores/account'
-import { mapStores } from 'pinia'
-
-export default {
-  computed: {
-    ...mapStores(useAccountStore, ['accountStore'])
-  }
-}
-</script>
+<script></script>
 
 <template>
   <div class="about">
     <!-- if there is a logged in user, display it. otherwise, say no one is logged in -->
-    <h1 v-if="accountStore.username">Logged in as {{ accountStore.username }}</h1>
-    <h1 v-else>No one is logged in</h1>
+    <!-- <h1 v-if="accountStore.username">Logged in as {{ accountStore.username }}</h1> -->
+    <!-- <h1 v-else>No one is logged in</h1> -->
+    <h1>No one is logged in.</h1>
   </div>
 </template>
 
--- frontend/src/views/UsersView.vue
+++ frontend/src/views/UsersView.vue
@@ -1,55 +1,10 @@
 <script>
 import axios from 'axios'
-import { mapStores } from 'pinia'
-import { useAccountStore } from '../stores/account'
 
 export default {
-  computed: {
-    ...mapStores(useAccountStore, ['accountStore'])
-  },
   data() {
     return {
-      users: [],
-      username: '',
-      email: '',
-      password: '',
-      error: ''
-    }
-  },
-  methods: {
-    async submitForm() {
-      try {
-        const { data: userData } = await axios.post('http://localhost:3000/users', {
-          username: this.username,
-          email: this.email,
-          password: this.password
-        })
-
-        // clear the input fields
-        this.username = ''
-        this.email = ''
-        this.password = ''
-
-        this.users.push(userData)
-
-        this.error = ''
-      } catch (error) {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          this.error = error.response.data.message
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          this.error = error.message
-        }
-      }
-    },
-    async login(username) {
-      if (this.accountStore.username) {
-        return
-      }
-
-      await this.accountStore.login(username)
+      users: []
     }
   },
   async mounted() {
@@ -66,25 +21,9 @@ export default {
 main
   h1 Users
 
-  h2 Register
-  form(@submit.prevent="submitForm")
-    div
-      label(for="username") Username:
-      input(type="text" id="username" v-model="username" required)
-    div
-      label(for="email") Email:
-      input(type="email" id="email" v-model="email" required)
-    div
-      label(for="password") Password:
-      input(type="password" id="password" v-model="password" required)
-    p.error(v-if="error") {{ error }}
-    button(type="submit") Submit
-
   h2 Signed up users
   div(v-for="user in users" :key="user.id")
     RouterLink(:to="{ name: 'user', params: { username: user.username } }") {{ user.username }}
-    // if user is not logged in, show login button. if logged in, show nothing
-    button(@click="login(user.username)" v-if="!accountStore.username") Login
 </template>
 
 <style lang="scss">
