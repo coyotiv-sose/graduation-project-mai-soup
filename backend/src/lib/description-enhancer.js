a0e81b64ab6fb1e2f583df324e61fdb89a3312f0
Maijs Garais <97809168+mai-soup@users.noreply.github.com>
2023-08-17 13:56:35 +0000

    increase token limit and explicitly say that the description should be short, to get full not partial responses

--- backend/src/lib/description-enhancer.js
+++ backend/src/lib/description-enhancer.js
@@ -5,12 +5,12 @@ const openai = new OpenAI({
 })
 
 const descriptionEnhancer = async description => {
-  const prompt = `Here is a description of a personal community library, please enhance it:\n\n"${description}"\n\nEnhanced description:\n\n`
+  const prompt = `Here is a description of a personal community library, please enhance it but keep it short:\n\n"${description}"\n\nEnhanced description:\n\n`
   const gptResponse = await openai.completions.create({
     model: 'text-davinci-003',
     prompt,
     temperature: 0.6,
-    max_tokens: 60,
+    max_tokens: 200,
   })
 
   return gptResponse.choices[0].text
