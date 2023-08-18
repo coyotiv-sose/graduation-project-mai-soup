63bd8f6d57ad59c2445f20b64c287eef12c9789c
Maijs Garais <97809168+mai-soup@users.noreply.github.com>
2023-08-18 20:11:41 +0000

    change error handler to send response instead of rendering view

--- backend/src/app.js
+++ backend/src/app.js
@@ -80,15 +80,14 @@ app.use((req, res, next) => {
 })
 
 // error handler
-app.use((err, req, res) => {
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  const { message } = err
+  const error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
   res.status(err.status || 500)
-  // TODO: send not render
-  res.render('error')
+  res.send({ message, error })
 })
 
 module.exports = app
