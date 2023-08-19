dc9c10f804e40e298bddea14bb9d2997ccf95d29
Maijs Garais <97809168+mai-soup@users.noreply.github.com>
2023-08-19 13:54:36 +0000

    show only libraries where the book is actually stored as removal candidates

--- frontend/src/views/BookView.vue
+++ frontend/src/views/BookView.vue
@@ -10,27 +10,29 @@ div(v-if="book")
         RouterLink(:to="{ name: 'library', params: { id: library._id } }") {{ library.name }}
 div(v-else) Loading...
 
-// if the logged in user owns any libraries, show a menu to add the book to one of them
-// if the logged in user doesn't own any libraries, show a message
-// if the book is already in a library, show a menu to remove it from that library
-
 div(v-if="ownedLibraries && ownedLibraries.length > 0")
   h2 Add to library
   select(v-model="libraryId")
     option(v-for="library in ownedLibraries" :key="library._id" :value="library._id") {{ library.name }}
   button(@click="doAddToLibrary") Add to library
+
+  h2 Remove from library
+  select(v-model="libraryId")
+    option(v-for="library in ownedLibrariesWithBook" :key="library._id" :value="library._id") {{ library.name }}
+  button(@click="doRemoveFromLibrary") Remove from library
 </template>
 
 <script>
 import axios from 'axios'
 import { useAccountStore } from '../stores/account'
-import { mapState } from 'pinia'
+import { mapActions, mapState } from 'pinia'
 
 export default {
   name: 'BookView',
   data() {
     return {
-      book: null
+      book: null,
+      libraryId: null
     }
   },
   created() {
@@ -41,18 +43,31 @@ export default {
       })
   },
   computed: {
-    ...mapState(useAccountStore, ['ownedLibraries'])
-  },
-  mounted() {
-    console.log('account', this.ownedLibraries)
+    ...mapState(useAccountStore, ['ownedLibraries']),
+    ownedLibrariesWithBook() {
+      if (this.book && this.ownedLibraries) {
+        return this.ownedLibraries.filter((library) => {
+          return library.books.some((b) => b.isbn === this.book.isbn)
+        })
+      }
+      return []
+    }
   },
   methods: {
+    ...mapActions(useAccountStore, ['fetchUser']),
     async doAddToLibrary() {
       await axios.post(
         `http://localhost:3000/libraries/${this.libraryId}/books`,
         {
           isbn: this.book.isbn
         }
+      )
+      await this.fetchUser()
+      this.$router.push({ name: 'library', params: { id: this.libraryId } })
+    },
+    async doRemoveFromLibrary() {
+      await axios.delete(
+        `http://localhost:3000/libraries/${this.libraryId}/books/${this.book.isbn}`
       )
       this.$router.push({ name: 'library', params: { id: this.libraryId } })
     }
