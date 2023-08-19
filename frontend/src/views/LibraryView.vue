c81d9c1363f6052c772c2dec1a565103b6123fc2
Maijs Garais <97809168+mai-soup@users.noreply.github.com>
2023-08-19 18:10:42 +0000

    fix library memberships not updating correctly in frontend until refresh

--- frontend/src/views/LibraryView.vue
+++ frontend/src/views/LibraryView.vue
@@ -5,6 +5,14 @@ div(v-else)
   p Location: {{ library.latitude }}, {{ library.longitude }}
   p Owner:
     RouterLink(v-if="ownerUsername" :to="{ name: 'user', params: { username: ownerUsername } }") {{ ownerUsername }}
+  // if logged in user is not the owner and is not a member, show the join button
+  button(v-if="isLoggedIn && ownerUsername !== username && !isUserMember" @click="join") Join
+  // if not the owner and is a member, show the leave button
+  button(v-if="isLoggedIn && ownerUsername !== username && isUserMember" @click="leave") Leave
+  h2 Members
+  ul
+    li(v-for="member in library.members" :key="member._id")
+      RouterLink(:to="{ name: 'user', params: { username: member._id } }") {{ member.username }}
   h2 Books
   ul
     li(v-for="book in library.books" :key="book.isbn")
@@ -14,6 +22,8 @@ div(v-else)
 <script>
 import axios from 'axios'
 import { RouterLink } from 'vue-router'
+import { useAccountStore } from '../stores/account'
+import { mapState } from 'pinia'
 
 export default {
   name: 'LibraryView',
@@ -28,11 +38,32 @@ export default {
   computed: {
     ownerUsername() {
       return this.library.owner ? this.library.owner.username : null
+    },
+    ...mapState(useAccountStore, ['isLoggedIn', 'username']),
+    isUserMember() {
+      return (
+        this.library &&
+        this.library.members.some((member) => member.username === this.username)
+      )
     }
   },
   async mounted() {
     const response = await axios.get(`/libraries/${this.$route.params.id}`)
     this.library = response.data
+  },
+  methods: {
+    async join() {
+      await axios.post(`/libraries/${this.$route.params.id}/members`)
+      this.library.members.push({ _id: this.username, username: this.username })
+    },
+    async leave() {
+      await axios.patch(`/libraries/${this.$route.params.id}/members`, {
+        remove: true
+      })
+      this.library.members = this.library.members.filter(
+        (member) => member.username !== this.username
+      )
+    }
   }
 }
 </script>
