4b5174475d663fa81bfdae983caab9d179e2d43f
Maijs Garais <mai-soup@mailfence.com>
2023-08-28 21:00:46 +0100

    show book info in a list, create component for items

--- frontend/src/views/GoogleBooksTest.vue
+++ frontend/src/views/GoogleBooksTest.vue
@@ -4,27 +4,34 @@ h1 Google Books API test
 form(v-on:submit.prevent="onSubmit")
   input(type="text" v-model="query")
   button(type="submit") Submit
+
+// if books not null, display a BookListItem component for each
+div(v-if="books")
+  BookListItem(v-for="book in books" :key="book.canonicalVolumeLink" :book="book")
 </template>
 
 <script>
 import axios from 'axios';
+import BookListItem from '../components/BookListItem.vue';
 
 export default {
   name: 'GoogleBooks',
+  components: {
+    BookListItem
+  },
   data () {
     return {
-      query: ''
+      query: '',
+      books: null
     }
   },
   methods: {
-    onSubmit () {
-      const results = axios.post('/books/google-books-search', {
+    async onSubmit () {
+      const results = await axios.post('/books/google-books-search', {
         query: this.query
       })
 
-      results.then(response => {
-        console.log(response.data)
-      })
+      this.books = results.data
     }
   }
 }
