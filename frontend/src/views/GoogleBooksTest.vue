90963383c92a9e4e60037ad0e69b32bb76e80141
Maijs Garais <mai-soup@mailfence.com>
2023-08-28 20:38:46 +0100

    add simple routes to test google books api

---
+++ frontend/src/views/GoogleBooksTest.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+h1 Google Books API test
+// form for query to search for
+form(v-on:submit.prevent="onSubmit")
+  input(type="text" v-model="query")
+  button(type="submit") Submit
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'GoogleBooks',
+  data () {
+    return {
+      query: ''
+    }
+  },
+  methods: {
+    onSubmit () {
+      const results = axios.post('/books/google-books-search', {
+        query: this.query
+      })
+
+      results.then(response => {
+        console.log(response.data)
+      })
+    }
+  }
+}
+</script>
