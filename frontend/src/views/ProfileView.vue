39eedbbcebd82d384e4c0de0d3bdc90135b58746
Maijs Garais <mai-soup@mailfence.com>
2023-09-01 16:25:43 +0100

    refactor profileview, remove unneeded information from being displayed

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -1,52 +1,26 @@
-<script>
-import axios from 'axios'
-import { mapActions, mapState } from 'pinia'
-import { useAccountStore } from '../stores/account'
-
-axios.baseURL = import.meta.env.VITE_API_URL
-
-export default {
-  computed: {
-    ...mapState(useAccountStore, ['user'])
-  },
-  methods: {
-    ...mapActions(useAccountStore, ['fetchUser']),
-    async doReturnBook(book) {
-      await axios.patch(`/copies/${book._id}`, {
-        action: 'return'
-      })
-      await this.fetchUser()
-    }
-  }
-}
-</script>
-
 <template lang="pug">
 .container
   div(v-if="!user") Loading...
   div(v-else)
     h1 {{ user.username }}
-    ul
-      li Email: {{ user.email }}
-      li Created at: {{ user.dateCreated }}
     h2 Memberships
     ul
       li(v-for="library in user.memberships" :key="library._id")
         RouterLink(:to="{ name: 'library', params: { id: library._id } }") {{ library.name }}
-    h2 Loans
-    table
-      thead
-        tr
-          th Title
-          th Status
-          th Action
-      tbody
-        tr(v-for="book in user.loans" :key="book._id")
-          td
-            RouterLink(:to="{ name: 'book', params: { isbn: book.bookInfo.isbn } }") {{ book.bookInfo.title }}
-          td
-            span(v-if="book.status === 'borrowed'") Borrowed from {{ book.library.name }} until {{ book.returnDate }}
-            span(v-else) {{ book.status }}
-          td
-            button(@click="doReturnBook(book)") Return
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'ProfileView',
+  data() {
+    return {
+      user: null
+    }
+  },
+  async mounted() {
+    this.user = (await axios.get(`/users/${this.$route.params.username}`)).data
+  },
+}
+</script>
