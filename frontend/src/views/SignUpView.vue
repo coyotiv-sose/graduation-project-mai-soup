9e3d4066ac2d8ee434713ccc13129c76ff01ae16
Maijs Garais <mai-soup@mailfence.com>
2023-09-04 17:54:17 +0100

    install email validator in frontend, add form validation to signup form

--- frontend/src/views/SignUpView.vue
+++ frontend/src/views/SignUpView.vue
@@ -1,14 +1,24 @@
 <script>
 import { mapActions } from 'pinia'
 import { useAccountStore } from '../stores/account'
+import validator from 'email-validator'
 
 export default {
   data() {
     return {
       username: '',
       email: '',
-      password: ''
+      password: '',
+      usernameError: null,
+      emailError: null,
+      // TODO: add password validation once implemented in backend
+      passwordError: null
     }
+  },
+  computed: {
+    shouldDisableSubmit() {
+      return this.usernameError || this.emailError || this.passwordError || !this.username || !this.email || !this.password
+  }
   },
   methods: {
     ...mapActions(useAccountStore, ['signUp']),
@@ -19,6 +29,42 @@ export default {
         password: this.password
       })
       this.$router.push('/login')
+    },
+    validateUsername(username) {
+      if (username.length < 3) {
+        this.usernameError = 'Username must be at least 3 characters'
+        return
+      }
+      
+      if (username.length > 24) {
+        this.usernameError = 'Username cannot exceed 24 characters'
+        return
+      } 
+      
+      if (!/^[a-zA-Z0-9_-]+$/.test(username)) {
+        this.usernameError = 'Username can only contain letters, numbers, underscores and dashes'
+        return
+      }
+
+      this.usernameError = null
+    },
+    validateEmail(email) {
+      if(!validator.validate(email)) {
+        this.emailError = 'Please enter a valid email address'
+        return
+      }
+
+      this.emailError = null
+    }
+  },
+  watch: {
+    username(value) {
+      this.username = value
+      this.validateUsername(value)
+    },
+    email(value) {
+      this.email = value
+      this.validateEmail(value)
     }
   }
 }
@@ -30,13 +76,15 @@ export default {
   form(@submit.prevent="performSignUp")
     div.form-group
       label(for="username") Username
-      input.form-control(type="text" id="username" v-model="username")
+      input.form-control(type="text" id="username" v-model="username" minlength="3" maxlength="24" pattern="^[a-zA-Z0-9_-]+$" required)
+      small(v-if="usernameError") {{ usernameError }}
     div.form-group
       label(for="email") Email
-      input.form-control(type="email" id="email" v-model="email")
+      input.form-control(type="email" id="email" v-model="email" required)
+      small(v-if="emailError") {{ emailError }}
     div.form-group
       label(for="password") Password
-      input.form-control(type="password" id="password" v-model="password")
+      input.form-control(type="password" id="password" v-model="password" required)
     div.form-group
-      button.btn.btn-primary(type="submit") Sign Up
+      button.btn.btn-primary(type="submit" :disabled="shouldDisableSubmit") Sign Up
 </template>
