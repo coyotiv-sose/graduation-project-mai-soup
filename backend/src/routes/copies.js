8c7bb1b02cff0e6964a5e9a8bf10a327df8c910a
Maijs Garais <mai-soup@mailfence.com>
2023-09-18 14:22:46 +0100

    refactor borrowing and returning books into handler, refactor patch backend route to be in libraries

--- backend/src/routes/copies.js
+++ backend/src/routes/copies.js
@@ -23,47 +23,4 @@ router.get('/:id', mustLogin, async (req, res, next) => {
   }
 })
 
-router.patch('/:id', mustLogin, async (req, res, next) => {
-  const { id } = req.params
-  const { action } = req.body
-  const { user } = req
-
-  if (!user) {
-    return next(createError(401, 'Unauthorized'))
-  }
-
-  const bookCopy = await BookCopy.findById(id)
-
-  if (!bookCopy) {
-    return next(createError(404, 'Book copy not found'))
-  }
-
-  try {
-    switch (action) {
-      case 'borrow':
-        await user.borrowBook(bookCopy)
-        return res.send(bookCopy)
-      case 'return':
-        await user.returnBook(bookCopy)
-        return res.send(bookCopy)
-      case 'extend':
-        await bookCopy.extend()
-        return res.send(bookCopy)
-      case 'lose':
-        await bookCopy.lose()
-        return res.send(bookCopy)
-      default:
-        return next(createError(400, 'Invalid action'))
-    }
-  } catch (err) {
-    console.error(err)
-    return next(
-      createError(
-        500,
-        'An error occurred while updating the book copy. Please try again later.'
-      )
-    )
-  }
-})
-
 module.exports = router
