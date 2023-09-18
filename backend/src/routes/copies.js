2cb5880d62c5c431834ac579b26d6881f8383776
Maijs Garais <mai-soup@mailfence.com>
2023-09-18 09:20:06 +0100

    remove unused book copy routes

--- backend/src/routes/copies.js
+++ backend/src/routes/copies.js
@@ -3,8 +3,6 @@ const express = require('express')
 const router = express.Router()
 const createError = require('http-errors')
 const mustLogin = require('../middleware/must-login')
-const Library = require('../models/library')
-const BookInfo = require('../models/book-info')
 const BookCopy = require('../models/book-copy')
 
 router.get('/:id', mustLogin, async (req, res, next) => {
@@ -18,58 +16,6 @@ router.get('/:id', mustLogin, async (req, res, next) => {
       createError(
         500,
         'An error occurred while retrieving the book copy. Please try again later.'
-      )
-    )
-  }
-})
-
-router.post('/:isbn', mustLogin, async (req, res, next) => {
-  const { isbn } = req.params
-  const { libraryId } = req.body
-  let bookInfo
-  let library
-
-  try {
-    bookInfo = await BookInfo.findOne({ isbn })
-  } catch (err) {
-    return next(
-      createError(
-        500,
-        'An error occurred while retrieving the book info. Please try again later.'
-      )
-    )
-  }
-
-  if (!bookInfo) {
-    return next(createError(404, 'Book info not found'))
-  }
-
-  try {
-    library = await Library.findById(libraryId)
-  } catch (err) {
-    return next(
-      createError(
-        500,
-        'An error occurred while retrieving the library. Please try again later.'
-      )
-    )
-  }
-
-  if (!library) {
-    return next(createError(404, 'Library not found'))
-  }
-
-  try {
-    const bookCopy = await BookCopy.create({
-      bookInfo,
-      library,
-    })
-    return res.status(201).send(bookCopy)
-  } catch (err) {
-    return next(
-      createError(
-        500,
-        'An error occurred while creating the book copy. Please try again later.'
       )
     )
   }
@@ -113,22 +59,6 @@ router.patch('/:id', mustLogin, async (req, res, next) => {
       createError(
         500,
         'An error occurred while updating the book copy. Please try again later.'
-      )
-    )
-  }
-})
-
-router.delete('/:id', mustLogin, async (req, res, next) => {
-  const { id } = req.params
-
-  try {
-    await BookCopy.findByIdAndDelete(id)
-    return res.status(204).send()
-  } catch (err) {
-    return next(
-      createError(
-        500,
-        'An error occurred while deleting the book copy. Please try again later.'
       )
     )
   }
