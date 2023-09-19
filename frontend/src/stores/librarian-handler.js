3a3f95ef5224dbdaefdf0848f1a881cbc0503f22
Maijs Garais <mai-soup@mailfence.com>
2023-09-19 13:43:32 +0100

    move library creation and updating to librarian handler, refactor library creation and editing views to use it

--- frontend/src/stores/librarian-handler.js
+++ frontend/src/stores/librarian-handler.js
@@ -20,6 +20,29 @@ export const useLibrarianHandler = defineStore('librarian-handler', {
         // TODO: handle error
         console.error(error)
       }
+    },
+    async createLibrary(name, location) {
+      try {
+        const response = await axios.post('/libraries', {
+          name,
+          location
+        })
+        return response.data
+      } catch (error) {
+        // TODO: handle error
+        console.error(error)
+      }
+    },
+    async updateLibrary(libraryId, name, location) {
+      try {
+        await axios.patch(`/libraries/${libraryId}`, {
+          name,
+          location
+        })
+      } catch (error) {
+        // TODO: handle error
+        console.error(error)
+      }
     }
   }
 })
