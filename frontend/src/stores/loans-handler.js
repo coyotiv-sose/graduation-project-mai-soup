808101ff6830a97f324a63e3d9460df9a5f2d776
Maijs Garais <mai-soup@mailfence.com>
2023-09-19 11:03:31 +0100

    create loans handler, move borrowing and returning book actions to it

---
+++ frontend/src/stores/loans-handler.js
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+export const useLoansHandler = defineStore('loans-handler', {
+  actions: {
+    async borrowBook(libraryId, bookCopyId) {
+      try {
+        await axios.patch(`/libraries/${libraryId}/copies/${bookCopyId}`, {
+          action: 'borrow'
+        })
+      } catch (error) {
+        // TODO: handle error
+        console.error(error)
+      }
+    },
+    async returnBook(libraryId, bookCopyId) {
+      try {
+        await axios.patch(`/libraries/${libraryId}/copies/${bookCopyId}`, {
+          action: 'return'
+        })
+      } catch (error) {
+        // TODO: handle error
+        console.error(error)
+      }
+    },
+    async extendLoan(libraryId, bookCopyId) {},
+    async fetchUsersLoans() {}
+  }
+})
