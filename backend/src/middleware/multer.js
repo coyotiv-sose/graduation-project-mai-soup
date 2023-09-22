c2e3fcc14cc8c4d036abb68e8b8638eb8d98dd4f
Maijs Garais <mai-soup@mailfence.com>
2023-09-22 12:01:29 +0100

    refactor error handling to middleware

--- backend/src/middleware/multer.js
+++ backend/src/middleware/multer.js
@@ -1,4 +1,5 @@
 const multer = require('multer')
+const createError = require('http-errors')
 
 const multerMiddleware = multer({
   storage: multer.memoryStorage(),
@@ -7,6 +8,21 @@ const multerMiddleware = multer({
   },
 })
 
-const singleFile = multerMiddleware.single('file')
+const singleFileMulter = multerMiddleware.single('file')
+
+const singleFile = (req, res, next) => {
+  // check if filesize isn't too large, throw 413 if it is
+  singleFileMulter(req, res, err => {
+    if (err instanceof multer.MulterError && err.code === 'LIMIT_FILE_SIZE') {
+      return next(createError(413, 'File too large'))
+    }
+
+    if (err) {
+      return next(createError(500, err.message))
+    }
+
+    return next()
+  })
+}
 
 module.exports = { singleFile }
