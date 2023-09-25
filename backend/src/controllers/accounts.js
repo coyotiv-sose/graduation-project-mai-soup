3790c21639b3255fd51f6f462343db30427eefa7
Maijs Garais <mai-soup@mailfence.com>
2023-09-25 12:50:30 +0100

    refactor accounts logic to controller

---
+++ backend/src/controllers/accounts.js
@@ -0,0 +1,33 @@
+const createError = require('http-errors')
+
+const User = require('../models/user')
+
+module.exports.registerUser = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body
+
+    const user = await User.register({ username, email }, password)
+
+    return res.send(user)
+  } catch (err) {
+    return next(createError(400, 'Registration failed'))
+  }
+}
+
+module.exports.getAuthenticatedUser = (req, res) => {
+  return res.send(req.user)
+}
+
+module.exports.logoutAndDestroySession = (req, res, next) => {
+  // return to appease the eslint overlords
+  return req.logout(err => {
+    if (err) return next(createError(500, 'Logout failed'))
+
+    // return to appease the eslint overlords
+    return req.session.destroy(error => {
+      return error
+        ? next(createError(500, "Couldn't destroy session"))
+        : res.sendStatus(200)
+    })
+  })
+}
