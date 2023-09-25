9c6727f1728ec804d7b72619677c7ee3cda556b7
Maijs Garais <mai-soup@mailfence.com>
2023-09-25 16:25:36 +0100

    replace inline js in LoansView with appropriate getters and actions in account store

--- frontend/src/stores/account.js
+++ frontend/src/stores/account.js
@@ -7,11 +7,22 @@ export const useAccountStore = defineStore('account', {
     username: (state) => state.user?.username,
     isLoggedIn: (state) => !!state.user,
     ownedLibraries: (state) => state.user?.ownedLibraries,
-    loans: (state) => state.user?.loans
+    loans: (state) => state.user?.loans,
+    hasLoans: (state) => !!state.user?.loans.length
   },
   actions: {
     async fetchUser() {
       this.user = (await axios.get('/accounts/session')).data
+
+      // add fields for whether the loan is about to expire
+      this.user.loans = this.user.loans.map((loan) => {
+        const loanReturnDate = new Date(loan.returnDate)
+        loan.isExpiringSoon =
+          loanReturnDate - new Date() < 1000 * 60 * 60 * 24 * 3
+        loan.isExpiringInAWeek =
+          loanReturnDate - new Date() < 1000 * 60 * 60 * 24 * 7
+        return loan
+      })
     },
     async login({ username, password }) {
       this.user = (
