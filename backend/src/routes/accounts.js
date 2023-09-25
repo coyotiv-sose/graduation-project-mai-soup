531bd44e64de9917dac29b34938cb366290cabd9
Maijs Garais <mai-soup@mailfence.com>
2023-09-25 12:50:30 +0100

    refactor accounts logic to controller

--- backend/src/routes/accounts.js
+++ backend/src/routes/accounts.js
@@ -1,46 +1,24 @@
 const express = require('express')
-const createError = require('http-errors')
 const passport = require('passport')
-const User = require('../models/user')
 
 const router = express.Router()
 
-router.post('/', async (req, res, next) => {
-  try {
-    const { username, email, password } = req.body
+const {
+  registerUser,
+  getAuthenticatedUser,
+  logoutAndDestroySession,
+} = require('../controllers/accounts')
 
-    const user = await User.register({ username, email }, password)
+router.post('/', registerUser)
 
-    return res.send(user)
-  } catch (err) {
-    return next(createError(400, 'Registration failed'))
-  }
-})
-
-router.get('/session', (req, res) => {
-  res.send(req.user)
-})
+router.get('/session', getAuthenticatedUser)
 
 router.post(
   '/session',
   passport.authenticate('local', { failWithError: true }),
-  (req, res) => {
-    res.send(req.user)
-  }
+  getAuthenticatedUser
 )
 
-router.delete('/session', (req, res, next) => {
-  // return to appease the eslint overlords
-  return req.logout(err => {
-    if (err) return next(createError(500, 'Logout failed'))
-
-    // return to appease the eslint overlords
-    return req.session.destroy(error => {
-      return error
-        ? next(createError(500, "Couldn't destroy session"))
-        : res.sendStatus(200)
-    })
-  })
-})
+router.delete('/session', logoutAndDestroySession)
 
 module.exports = router
