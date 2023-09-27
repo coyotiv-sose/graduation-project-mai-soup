ac72a7e960a214876694621971020a4c77fb3f8f
Maijs Garais <mai-soup@mailfence.com>
2023-09-27 16:32:58 +0100

    move remaining account-related tests to corresponding suite

--- backend/tests/routes/accounts.test.js
+++ backend/tests/routes/accounts.test.js
@@ -2,6 +2,8 @@ const request = require('supertest')
 const chance = require('chance').Chance()
 const app = require('../../src/app')
 
+const User = require('../../src/models/user')
+
 // use agent to persist cookies between requests
 const agent = request.agent(app)
 const validUsername = chance.word()
@@ -81,6 +83,35 @@ it('should not sign up a user with an existing email', async () => {
 //   expect(response.status).toBe(400)
 // })
 
+it('should not sign up a user with no username', async () => {
+  const response = await request(app).post('/accounts').send({
+    username: '',
+    email: chance.email(),
+    password: chance.word(),
+  })
+
+  expect(response.status).toBe(400)
+})
+
+it('should not sign up a user with no email', async () => {
+  const response = await request(app).post('/accounts').send({
+    username: chance.word(),
+    email: '',
+    password: chance.word(),
+  })
+
+  expect(response.status).toBe(400)
+})
+
+it('should not sign up a user with no password', async () => {
+  const response = await request(app).post('/accounts').send({
+    username: chance.word(),
+    email: chance.email(),
+    password: '',
+  })
+  expect(response.status).toBe(400)
+})
+
 it('should sign in a user', async () => {
   const response = await agent.post('/accounts/session').send({
     username: validUsername,
@@ -103,7 +134,7 @@ it('should not sign in a user with an invalid username', async () => {
   expect(response.status).toBe(401)
 })
 
-it('should not sign in a user with an invalid password', async () => {
+it('should not sign in a user with an wrong password', async () => {
   const response = await request(app).post('/accounts/session').send({
     username: validUsername,
     password: chance.word(),
@@ -121,6 +152,15 @@ it('should not sign in a user with an invalid email', async () => {
   expect(response.status).toBe(401)
 })
 
+it('should not sign in a user without a password', async () => {
+  const response = await request(app).post('/accounts/session').send({
+    username: validUsername,
+    password: '',
+  })
+
+  expect(response.status).toBe(400)
+})
+
 it('should sign out a user', async () => {
   const response = await agent.delete('/accounts/session')
   expect(response.status).toBe(200)
@@ -129,4 +169,18 @@ it('should sign out a user', async () => {
 it('should not sign out a user if not signed in', async () => {
   const response = await request(app).delete('/accounts/session')
   expect(response.status).toBe(401)
+})
+
+it('should handle server errors when signing up a new user', async () => {
+  User.register = jest.fn().mockRejectedValueOnce(new Error('Simulated Error'))
+
+  const user = {
+    username: chance.word(),
+    email: chance.email(),
+    password: chance.word(),
+  }
+
+  const response = await request(app).post('/accounts').send(user)
+  // console.error('BODY:', response.text)
+  expect(response.status).toBe(500)
 })
