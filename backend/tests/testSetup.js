7d8d59c70b495f9fd2dd291fba9a1be8a6aa99cd
Maijs Garais <mai-soup@mailfence.com>
2023-09-28 18:33:13 +0100

    specify db name for tests, change dropping each collection before all tests to dropping whole db

--- backend/tests/testSetup.js
+++ backend/tests/testSetup.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose')
 const dbConnection = require('../src/database-connection')
-const User = require('../src/models/user')
-const BookInfo = require('../src/models/book-info')
-const Library = require('../src/models/library')
 
 beforeAll(async () => {
-  await dbConnection
-  await User.deleteMany({})
-  await BookInfo.deleteMany({})
-  await Library.deleteMany({})
+  const connection = await dbConnection
+  await connection.dropDatabase()
 })
 
 afterAll(() => {
