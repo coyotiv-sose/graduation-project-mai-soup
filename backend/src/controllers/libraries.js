759f9922062c4fe66111d1f16998620cec3e7ffd
Maijs Garais <mai-soup@mailfence.com>
2023-10-03 14:18:57 +0100

    finish library route tests, fix library controllers try/catch scope, send updated book copy upon patching

--- backend/src/controllers/libraries.js
+++ backend/src/controllers/libraries.js
@@ -74,50 +74,50 @@ module.exports.addCopy = async (req, res, next) => {
   const { id } = req.params
   const { openLibraryId } = req.body
 
-  const library = await Library.findById(id)
-  if (!library) return next(createError(404, 'Library not found'))
-
-  // only library owner can add copies
-  const { user } = req
-  if (user.id !== library.owner.id)
-    return next(createError(403, 'You are not the owner of this library'))
-
-  let book = await BookInfo.findOne({ openLibraryId })
-  if (!book) {
-    // add from OpenLibrary API if not found locally
-    const volume = await axios.get(
-      `https://openlibrary.org/works/${openLibraryId}.json`
-    )
-    // TODO: handle errors
-
-    const { title, covers, authors } = volume.data
-
-    // convert author objects to author names
-    const authorIds = authors.map(item => item.author.key)
-
-    const authorNames = await Promise.all(
-      authorIds.map(async a => {
-        const author = await axios.get(`https://openlibrary.org${a}.json`)
-        console.error(a)
-        return author.data.name
-      })
-    )
-    // construct bookInfo
-    // TODO: error handling
-    book = await BookInfo.create({
-      openLibraryId,
-      title,
-      authors: [...new Set(authorNames)], // remove duplicate authors
-      imageUrl: covers
-        ? `https://covers.openlibrary.org/b/id/${covers[0]}-M.jpg`
-        : null,
-    })
-  }
-
   try {
+    const library = await Library.findById(id)
+    if (!library) return next(createError(404, 'Library not found'))
+
+    // only library owner can add copies
+    const { user } = req
+    if (user.id !== library.owner.id)
+      return next(createError(403, 'You are not the owner of this library'))
+
+    let book = await BookInfo.findOne({ openLibraryId })
+    if (!book) {
+      // add from OpenLibrary API if not found locally
+      const volume = await axios.get(
+        `https://openlibrary.org/works/${openLibraryId}.json`
+      )
+      // TODO: handle errors
+
+      const { title, covers, authors } = volume.data
+
+      // convert author objects to author names
+      const authorIds = authors.map(item => item.author.key)
+
+      const authorNames = await Promise.all(
+        authorIds.map(async a => {
+          const author = await axios.get(`https://openlibrary.org${a}.json`)
+          console.error(a)
+          return author.data.name
+        })
+      )
+      // construct bookInfo
+      // TODO: error handling
+      book = await BookInfo.create({
+        openLibraryId,
+        title,
+        authors: [...new Set(authorNames)], // remove duplicate authors
+        imageUrl: covers
+          ? `https://covers.openlibrary.org/b/id/${covers[0]}-M.jpg`
+          : null,
+      })
+    }
     await library.addBook(book)
     return res.status(201).send(library)
   } catch (err) {
+    console.error(err)
     return next(createError(500, err.message))
   }
 }
@@ -125,19 +125,19 @@ module.exports.addCopy = async (req, res, next) => {
 module.exports.removeCopy = async (req, res, next) => {
   const { id, bookId } = req.params
 
-  const library = await Library.findById(id)
-  if (!library) return next(createError(404, 'Library not found'))
-
-  // only library owner can remove copies
-  // TODO: refactor to middleware
-  const { user } = req
-  if (user.id !== library.owner.id)
-    return next(createError(403, 'You are not the owner of this library'))
-
-  const book = await BookCopy.findById(bookId)
-  if (!book) return next(createError(404, 'Book not found'))
-
   try {
+    const library = await Library.findById(id)
+    if (!library) return next(createError(404, 'Library not found'))
+
+    // only library owner can remove copies
+    // TODO: refactor to middleware
+    const { user } = req
+    if (user.id !== library.owner.id)
+      return next(createError(403, 'You are not the owner of this library'))
+
+    const book = await BookCopy.findById(bookId)
+    if (!book) return next(createError(404, 'Book not found'))
+
     await library.removeBookCopy(book)
     return res.status(204).send()
   } catch (err) {
@@ -202,29 +202,35 @@ module.exports.updateCopy = async (req, res, next) => {
   const { action } = req.body
   const { user } = req
 
-  const bookCopy = await BookCopy.findById(copyId)
-
-  if (!bookCopy) {
-    return next(createError(404, 'Book copy not found'))
-  }
-
   try {
-    switch (action) {
-      case 'borrow':
-        await user.borrowBook(bookCopy)
-        return res.send(bookCopy)
-      case 'return':
-        await user.returnBook(bookCopy)
-        return res.send(bookCopy)
-      case 'extend':
-        await bookCopy.extend()
-        return res.send(bookCopy)
-      case 'lose':
-        await bookCopy.lose()
-        return res.send(bookCopy)
-      default:
-        return next(createError(400, 'Invalid action'))
+    const bookCopy = await BookCopy.findById(copyId)
+
+    if (!bookCopy) {
+      return next(createError(404, 'Book copy not found'))
     }
+
+    try {
+      switch (action) {
+        case 'borrow':
+          await user.borrowBook(bookCopy)
+          break
+        case 'return':
+          await user.returnBook(bookCopy)
+          break
+        case 'extend':
+          await bookCopy.extend()
+          break
+        case 'lose':
+          await bookCopy.lose()
+          break
+        default:
+          return next(createError(400, 'Invalid action'))
+      }
+    } catch (err) {
+      return next(createError(403, err.message))
+    }
+    const updatedCopy = await BookCopy.findById(copyId)
+    return res.send(updatedCopy)
   } catch (err) {
     console.error(err)
     return next(
