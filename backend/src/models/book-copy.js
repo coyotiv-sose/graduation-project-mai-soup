c627e1a631252a6259caf9296ef0124bd3dce2ea
Maijs Garais <mai-soup@mailfence.com>
2023-10-04 12:42:07 +0100

    refactor services into their own files

--- backend/src/models/book-copy.js
+++ backend/src/models/book-copy.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const autopopulate = require('mongoose-autopopulate')
+const BookCopyService = require('../services/book-copy')
 
 const bookCopySchema = new mongoose.Schema({
   bookInfo: {
@@ -33,54 +34,7 @@ const bookCopySchema = new mongoose.Schema({
   // how to be sure that the correct copy is updated if there are several? difficult.
 })
 
-class BookCopy {
-  async borrow(user) {
-    if (this.status !== 'available') {
-      throw new Error('book is not available')
-    }
-
-    this.status = 'borrowed'
-    this.borrower = user
-    this.returnDate = new Date(Date.now() + 14 * 24 * 60 * 60 * 1000) // 14 days from now
-    await this.save()
-  }
-
-  async return() {
-    if (this.status !== 'borrowed') {
-      throw new Error('book is not borrowed')
-    }
-
-    this.status = 'available'
-    this.borrower = null
-    this.returnDate = null
-    await this.save()
-  }
-
-  async extend() {
-    if (this.status !== 'borrowed') {
-      throw new Error('book is not borrowed')
-    }
-
-    // if the book's return date is more than 7 days from now, don't extend
-    if (this.returnDate.getTime() - Date.now() > 7 * 24 * 60 * 60 * 1000) {
-      throw new Error('return date is too far in the future')
-    }
-
-    this.returnDate = new Date(
-      this.returnDate.getTime() + 7 * 24 * 60 * 60 * 1000
-    ) // 7 days from now
-    await this.save()
-  }
-
-  async lose() {
-    this.status = 'lost'
-    this.borrower = null
-    this.returnDate = null
-    await this.save()
-  }
-}
-
-bookCopySchema.loadClass(BookCopy)
+bookCopySchema.loadClass(BookCopyService)
 bookCopySchema.plugin(autopopulate)
 
 module.exports = mongoose.model('BookCopy', bookCopySchema)
---
+++ backend/src/services/user.js
@@ -0,0 +1,68 @@
+const Library = require('../models/library')
+const BookCopy = require('../models/book-copy')
+
+class UserService {
+  async createLibrary({ name, latitude, longitude }) {
+    const newLibrary = await Library.create({
+      name,
+      owner: this,
+      latitude,
+      longitude,
+    })
+
+    this.memberships.push(newLibrary)
+    await this.save()
+    await newLibrary.save()
+    return newLibrary
+  }
+
+  async joinLibrary(library) {
+    this.memberships.push(library)
+    await this.save()
+    await library.addMember(this)
+  }
+
+  async leaveLibrary(library) {
+    const libraryId = library._id.toString()
+    const membershipIds = this.memberships.map(m => m._id.toString())
+
+    if (!membershipIds.includes(libraryId)) {
+      throw new Error('user is not member of this library')
+    }
+
+    const libraryIndex = membershipIds.indexOf(libraryId)
+    this.memberships.splice(libraryIndex, 1)
+
+    await this.save()
+    await library.removeMember(this)
+  }
+
+  async borrowBook(bookCopy) {
+    if (bookCopy.status !== 'available') {
+      throw new Error('book copy is not available')
+    }
+
+    await bookCopy.borrow(this)
+    this.loans.push(bookCopy)
+    await this.save()
+  }
+
+  async returnBook(bookCopy) {
+    const bookCopyObj = await BookCopy.findById(bookCopy._id)
+
+    if (
+      bookCopyObj.status !== 'borrowed' ||
+      bookCopyObj.borrower._id.toString() !== this._id.toString()
+    ) {
+      throw new Error('book copy is not borrowed by this user')
+    }
+
+    await bookCopyObj.return()
+    const bookCopyId = bookCopyObj._id.toString()
+    // filter out bookCopy from loans
+    this.loans = this.loans.filter(loan => loan._id.toString() !== bookCopyId)
+    await this.save()
+  }
+}
+
+module.exports = UserService
