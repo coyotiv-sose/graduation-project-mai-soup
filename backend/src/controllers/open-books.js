a15fb080e32b86c8ea4ce9a7f8b23f41ba214a04
Maijs Garais <mai-soup@mailfence.com>
2023-10-16 19:56:07 +0100

    refactor open books controller to use catchAsync

--- backend/src/controllers/open-books.js
+++ backend/src/controllers/open-books.js
@@ -1,25 +1,28 @@
 const axios = require('axios')
 const createError = require('http-errors')
+const catchAsync = require('../utils/catch-async')
 
-module.exports.getOpenLibraryVolumesByQuery = async (req, res, next) => {
-  const query = req.query.q
+module.exports.getOpenLibraryVolumesByQuery = catchAsync(
+  async (req, res, next) => {
+    const query = req.query.q
 
-  if (!query) return next(createError(400, 'Missing query'))
+    if (!query) return next(createError(400, 'Missing query'))
 
-  const response = (
-    await axios.get(`https://openlibrary.org/search.json?q=${query}&limit=20`)
-  ).data.docs
+    const response = (
+      await axios.get(`https://openlibrary.org/search.json?q=${query}&limit=20`)
+    ).data.docs
 
-  if (!response.length) return next(createError(404, 'No results found'))
+    if (!response.length) return next(createError(404, 'No results found'))
 
-  const results = response.map(book => ({
-    title: book.title,
-    authors: book.author_name?.join(', ') || null,
-    id: book.key.replace('/works/', ''),
-    coverUrl: book.cover_i
-      ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
-      : null,
-  }))
+    const results = response.map(book => ({
+      title: book.title,
+      authors: book.author_name?.join(', ') || null,
+      id: book.key.replace('/works/', ''),
+      coverUrl: book.cover_i
+        ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
+        : null,
+    }))
 
-  return res.send(results)
-}
+    return res.send(results)
+  }
+)
