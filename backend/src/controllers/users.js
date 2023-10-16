90be2f0bd9b0b1dc3bdee326f8849dc8dd9fda8f
Maijs Garais <mai-soup@mailfence.com>
2023-10-16 19:14:31 +0100

    use catchAsync in user controller

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -1,25 +1,16 @@
 const createError = require('http-errors')
-
+const catchAsync = require('../utils/catch-async')
 const User = require('../models/user')
 
-module.exports.getUserByUsername = async (req, res, next) => {
+module.exports.getUserByUsername = catchAsync(async (req, res, next) => {
   const { username } = req.params
 
-  try {
-    const user = await User.findOne(
-      { username },
-      'username memberships ownedLibraries loans'
-    )
+  const user = await User.findOne(
+    { username },
+    'username memberships ownedLibraries loans'
+  )
 
-    if (!user) return next(createError(404, 'User not found'))
+  if (!user) return next(createError(404, 'User not found'))
 
-    return res.send(user)
-  } catch (error) {
-    return next(
-      createError(
-        500,
-        'An error occurred while retrieving the user. Please try again later.'
-      )
-    )
-  }
-}
+  return res.send(user)
+})
