7cc5529dfa070126726039d2cc3a3ddf6a24576f
Maijs Garais <mai-soup@mailfence.com>
2024-01-08 21:56:27 +0000

    fix book route tests

--- backend/tests/routes/books.test.js
+++ backend/tests/routes/books.test.js
@@ -1,15 +1,11 @@
 const request = require('supertest')
 const chance = require('chance').Chance()
 const app = require('../../src/app')
-const BookInfo = require('../../src/models/book-info')
+const mongoose = require('mongoose')
+const Book = require('../../src/models/book')
 const getValidPassword = require('../generateValidPassword')
 
 const agent = request.agent(app)
-
-// donna tartt's goldfinch
-const validOpenLibraryId = 'OL16809803W'
-// arthur conan doyle's adventures of gerard
-const anotherValidOpenLibraryId = 'OL262595W'
 
 beforeAll(async () => {
   const username = chance.word({ length: 10 })
@@ -25,9 +21,6 @@ beforeAll(async () => {
     username,
     password,
   })
-
-  // await book info to be deleted because tests depend on unique keys
-  await BookInfo.deleteMany({})
 })
 
 it('should not allow getting all books without auth', async () => {
@@ -40,38 +33,17 @@ it('should not allow getting a single book without auth', async () => {
   expect(response.status).toBe(401)
 })
 
-it('should not allow creating a new book without auth', async () => {
+it('should not allow creating a new book through this route', async () => {
   const response = await request(app).post('/books')
-  expect(response.status).toBe(401)
+  expect(response.status).toBe(404)
 })
 
-it('should create a new book with a valid openLibraryId', async () => {
-  const response = await agent.post('/books').send({ id: validOpenLibraryId })
-  expect(response.status).toBe(201)
-  expect(response.body.openLibraryId).toBe(validOpenLibraryId)
-})
-
-it('should not create a new book without an id', async () => {
-  const response = await agent.post('/books').send({})
-  expect(response.status).toBe(400)
-})
-
-// TODO: tests for invalid id and api being down
-
-it('should not create a new book with a duplicate id', async () => {
-  await agent.post('/books').send({ id: anotherValidOpenLibraryId })
-  const response = await agent
-    .post('/books')
-    .send({ id: anotherValidOpenLibraryId })
-  expect(response.status).toBe(409)
-})
-
-it('should send 500 if encountering server error', async () => {
-  jest.spyOn(BookInfo, 'findOne').mockImplementationOnce(() => {
+it('should send 500 if encountering server error on getting single book', async () => {
+  jest.spyOn(Book, 'findOne').mockImplementationOnce(() => {
     throw new Error('error')
   })
 
-  const response = await agent.post('/books').send({ id: validOpenLibraryId })
+  const response = await agent.get('/books/1')
   expect(response.status).toBe(500)
 })
 
@@ -81,7 +53,7 @@ it('should get all books', async () => {
 })
 
 it('should return 500 upon server error when getting all books', async () => {
-  jest.spyOn(BookInfo, 'find').mockImplementationOnce(() => {
+  jest.spyOn(Book, 'find').mockImplementationOnce(() => {
     throw new Error('error')
   })
 
@@ -89,59 +61,27 @@ it('should return 500 upon server error when getting all books', async () => {
   expect(response.status).toBe(500)
 })
 
-it('should find books by title', async () => {
-  const title = 'Goldfinch'
-  const response = await agent.get(`/books?q=${title}`)
-
-  expect(response.status).toBe(200)
-  expect(response.body).toEqual(
-    expect.arrayContaining([
-      expect.objectContaining({
-        openLibraryId: validOpenLibraryId,
-        title: expect.stringMatching(title),
-      }),
-    ])
-  )
-})
-
-it('should find books by author', async () => {
-  const author = 'Tartt'
-  const response = await agent.get(`/books?q=${author}`)
-
-  expect(response.status).toBe(200)
-  expect(response.body).toEqual(
-    expect.arrayContaining([
-      expect.objectContaining({
-        openLibraryId: validOpenLibraryId,
-        authors: expect.arrayContaining([expect.stringMatching(author)]),
-      }),
-    ])
-  )
-})
-
-it('should return 404 when searching for a query that is not in db', async () => {
-  const fakeQuery = chance.word().repeat(10)
-  const response = await agent.get(`/books?q=${fakeQuery}`)
-
-  expect(response.status).toBe(404)
-})
-
 it('should get a single book', async () => {
-  const response = await agent.get(`/books/${validOpenLibraryId}`)
+  const testTitle = chance.word()
+  const testAuthors = chance.word()
+  const fakeId = new mongoose.Types.ObjectId()
+
+  const book = await Book.create({
+    title: testTitle,
+    authors: testAuthors,
+    library: fakeId,
+  })
+
+  const response = await agent.get(`/books/${book._id}`)
   expect(response.status).toBe(200)
-  expect(response.body.openLibraryId).toBe(validOpenLibraryId)
+  expect(response.body).toMatchObject({
+    title: testTitle,
+    authors: testAuthors,
+  })
 })
 
 it('should return not found on non-existent book id', async () => {
-  const response = await agent.get(`/books/${chance.word()}`)
+  const fakeId = new mongoose.Types.ObjectId()
+  const response = await agent.get(`/books/${fakeId}`)
   expect(response.status).toBe(404)
-})
-
-it('should return 500 upon server error when getting a single book', async () => {
-  jest.spyOn(BookInfo, 'findOne').mockImplementationOnce(() => {
-    throw new Error('error')
-  })
-
-  const response = await agent.get(`/books/${validOpenLibraryId}`)
-  expect(response.status).toBe(500)
 })
