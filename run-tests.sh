1f8c919af38122704062e6030a7f90ae16fc3976
Maijs Garais <mai-soup@mailfence.com>
2024-07-16 15:33:21 +0100

    add migration dockerfile to test run script, rename Dockerfile to be consistent

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-docker compose -f docker-compose.test.yml --project-name test up --build --abort-on-container-exit
+
+# defaults
+COMPOSE_FILE="docker-compose.test.yml"
+PROJECT_NAME="test"
+
+if [ "$1" = "migration" ]; then
+  COMPOSE_FILE="docker-compose.migration-test.yml"
+  PROJECT_NAME="test-migration"
+fi
+
+echo "Using compose file: $COMPOSE_FILE"
+
+docker compose -f $COMPOSE_FILE --project-name $PROJECT_NAME up --build --abort-on-container-exit
