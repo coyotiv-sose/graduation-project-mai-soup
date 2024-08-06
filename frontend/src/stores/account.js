8aa9fa2f41dc035ce1ee654b84afa93a53fdf831
Maijs Garais <mai-soup@mailfence.com>
2024-08-06 12:57:08 +0100

    migrate all account handler reqs to composable

--- frontend/src/stores/account.js
+++ frontend/src/stores/account.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import axios from 'axios'
+import useApiRequests from '../composables/useApiRequests'
 
 export const useAccountStore = defineStore('account', {
   state: () => ({ user: null }),
@@ -11,44 +11,47 @@ export const useAccountStore = defineStore('account', {
     hasLoans: (state) => !!state.user?.loans.length
   },
   actions: {
-    async fetchUser() {
-      this.user = (await axios.get('/accounts/session')).data
+    ...(() => {
+      const { get, post, del, patch } = useApiRequests()
+      return {
+        async fetchUser() {
+          this.user = await get('/accounts/session')
 
-      // add fields for whether the loan is about to expire
-      if (this.user) {
-        this.user.loans = this.user.loans.map((loan) => {
-          const loanReturnDate = new Date(loan.returnDate)
-          loan.isExpiringSoon =
-            loanReturnDate - new Date() < 1000 * 60 * 60 * 24 * 3
-          loan.isExpiringInAWeek =
-            loanReturnDate - new Date() < 1000 * 60 * 60 * 24 * 7
-          return loan
-        })
+          // add fields for whether the loan is about to expire
+          if (this.user) {
+            this.user.loans = this.user.loans.map((loan) => {
+              const loanReturnDate = new Date(loan.returnDate)
+              loan.isExpiringSoon =
+                loanReturnDate - new Date() < 1000 * 60 * 60 * 24 * 3
+              loan.isExpiringInAWeek =
+                loanReturnDate - new Date() < 1000 * 60 * 60 * 24 * 7
+              return loan
+            })
+          }
+        },
+        async login({ username, password }) {
+          this.user = await post('/accounts/session', { username, password })
+        },
+        async logout() {
+          await del('/accounts/session')
+          this.user = null
+
+          if (this.$router.currentRoute.value.meta.requiresAuth) {
+            // if current route requires auth, redirect to login
+            this.$router.push({ name: 'login' })
+          }
+        },
+        async signUp({ username, email, password }) {
+          await post('/accounts', { username, email, password })
+        },
+        isOwnerOfLibrary(libraryId) {
+          if (!this.isLoggedIn) return false
+
+          return this.user.ownedLibraries?.some(
+            (library) => library._id === libraryId
+          )
+        }
       }
-    },
-    async login({ username, password }) {
-      this.user = (
-        await axios.post('/accounts/session', { username, password })
-      ).data
-    },
-    async logout() {
-      await axios.delete('/accounts/session')
-      this.user = null
-
-      if (this.$router.currentRoute.value.meta.requiresAuth) {
-        // if current route requires auth, redirect to login
-        this.$router.push({ name: 'login' })
-      }
-    },
-    async signUp({ username, email, password }) {
-      await axios.post('/accounts', { username, email, password })
-    },
-    isOwnerOfLibrary(libraryId) {
-      if (!this.isLoggedIn) return false
-
-      return this.user.ownedLibraries?.some(
-        (library) => library._id === libraryId
-      )
-    }
+    })()
   }
 })
