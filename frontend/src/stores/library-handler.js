3bb7c94b1a0fcc0d5c4c077c4d0951a21ee7ac1b
Maijs Garais <mai-soup@mailfence.com>
2024-08-06 12:46:15 +0100

    replace axios get methods in library handler with composable

--- frontend/src/stores/library-handler.js
+++ frontend/src/stores/library-handler.js
@@ -1,43 +1,42 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
+import useApiRequests from '../composables/useApiRequests'
 
 export const useLibraryHandler = defineStore('library-handler', {
   actions: {
-    async fetchAllLibraries() {
-      try {
-        const response = await axios.get('/libraries')
-        return response.data
-      } catch (error) {
-        // TODO: handle error
-        console.error(error)
+    // Immediately Invoked Function Expression (IIFE) to set up API request methods
+    // the IIFE allows to initialize the `get` method from the `useApiRequests` composable only once
+    // and then use them in the store actions without repeating the initialization code
+    ...(() => {
+      const { get } = useApiRequests()
+      return {
+        async fetchAllLibraries() {
+          const libraries = await get('/libraries')
+          return libraries
+        },
+        async fetchLibrary(libraryId) {
+          const libraries = await get(`/libraries/${libraryId}`)
+          return libraries
+        },
+        async joinLibrary(libraryId) {
+          try {
+            await axios.post(`/libraries/${libraryId}/members`)
+          } catch (error) {
+            // TODO: handle error
+            console.error(error)
+          }
+        },
+        async leaveLibrary(libraryId) {
+          try {
+            await axios.patch(`/libraries/${libraryId}/members`, {
+              remove: true
+            })
+          } catch (error) {
+            // TODO: handle error
+            console.error(error)
+          }
+        }
       }
-    },
-    async fetchLibrary(libraryId) {
-      try {
-        const response = await axios.get(`/libraries/${libraryId}`)
-        return response.data
-      } catch (error) {
-        // TODO: handle error
-        console.error(error)
-      }
-    },
-    async joinLibrary(libraryId) {
-      try {
-        await axios.post(`/libraries/${libraryId}/members`)
-      } catch (error) {
-        // TODO: handle error
-        console.error(error)
-      }
-    },
-    async leaveLibrary(libraryId) {
-      try {
-        await axios.patch(`/libraries/${libraryId}/members`, {
-          remove: true
-        })
-      } catch (error) {
-        // TODO: handle error
-        console.error(error)
-      }
-    }
+    })()
   }
 })
