31d1b6a764546d419bfd2acf0e46af777493319e
Maijs Garais <mai-soup@mailfence.com>
2024-08-07 18:30:36 +0100

    extract logic for checking whether a book exists into its own middleware

--- backend/src/controllers/books.js
+++ backend/src/controllers/books.js
@@ -1,4 +1,3 @@
-const createError = require('http-errors')
 const catchAsync = require('../utils/catch-async')
 const Book = require('../models/book')
 
@@ -6,7 +5,6 @@ module.exports.getSingleBook = catchAsync(async (req, res, next) => {
   const { id } = req.params
   const book = await Book.findById(id)
 
-  if (!book) return next(createError(404, 'Book not found'))
   return res.send(book)
 })
 
--- backend/src/routes/books.js
+++ backend/src/routes/books.js
@@ -2,10 +2,11 @@ const express = require('express')
 
 const router = express.Router()
 const mustLogin = require('../middleware/must-login')
+const bookExists = require('../middleware/book-exists')
 const { getAllBooks, getSingleBook } = require('../controllers/books')
 
 router.get('/', mustLogin, getAllBooks)
 
-router.get('/:id', mustLogin, getSingleBook)
+router.get('/:id', mustLogin, bookExists, getSingleBook)
 
 module.exports = router
