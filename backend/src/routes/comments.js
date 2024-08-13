38b90a10c9d0006194ae94e18155c985bb63013f
Maijs Garais <mai-soup@mailfence.com>
2024-08-13 14:41:23 +0100

    backend linting

--- backend/src/routes/comments.js
+++ backend/src/routes/comments.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const router = express.Router({ mergeParams: true })
 const mustLogin = require('../middleware/must-login')
+
+const router = express.Router({ mergeParams: true })
 
 const {
   getLibraryComments,
