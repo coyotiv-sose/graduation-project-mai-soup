6ff3ee0493de82c2012b914882a6b76a4a03fc7d
Maijs Garais <mai-soup@mailfence.com>
2024-08-19 22:40:57 +0100

    start work on home view

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,25 +1,36 @@
 <template lang="pug">
+header
+  nav.navbar(role='navigation' aria-label='main navigation')
+    .navbar-brand
+      RouterLink.navbar-item.has-text-weight-bold.is-size-5(to='/') Porch Reads Club
+      //- TODO: toggle burger and menu on mobile
+      a.navbar-burger(role='button' aria-label='menu' aria-expanded='false' data-target='nav')
+        span(aria-hidden='true')
+        span(aria-hidden='true')
+        span(aria-hidden='true')
+        span(aria-hidden='true')
+    .navbar-menu#nav
+      .navbar-start
+        RouterLink.navbar-item.active(to='/') Home
+        RouterLink.navbar-item(to='/libraries') Libraries
+        RouterLink.navbar-item(v-if='isLoggedIn' to='/books') Books
+      .navbar-end
+        RouterLink.navbar-item(v-if='!isLoggedIn' to='/login') Login
+        RouterLink.navbar-item(v-if='!isLoggedIn' to='/signup') Sign Up
+        a.navbar-item(v-if='isLoggedIn' @click='doLogout') Logout
 .bg
-  .container
-    header
-      span.title Porch Reads Club
-      nav
-        RouterLink.active(to='/') Home
-        RouterLink(to='/libraries') Libraries
-        RouterLink(v-if='isLoggedIn' to='/books') Books
-        RouterLink(v-if='!isLoggedIn' to='/login') Login
-        RouterLink(v-if='!isLoggedIn' to='/signup') Sign Up
-        a(v-if='isLoggedIn' @click='doLogout') Logout
-    main
-      h1 Porch Reads Club
-      p Welcome to your online library hub!
-      p
-        | Simplify book management, connect with members,
-        br
+  div
+  .hero
+    .hero-body
+      p.title.mb-5  Porch Reads Club
+      p.subtitle Welcome to your online library hub!
+      p.subtitle Simplify book management, connect with members, 
+        br 
         | and enhance your book lending experience.
-      p Dive in and explore libraries now!
-      RouterLink.action-btn(to='/libraries') View Libraries
-    footer Maijs Garais, 2023
+      p.subtitle Dive in and explore libraries now!
+      RouterLink.button.is-primary(to='/libraries') View Libraries
+  footer
+    p.mb-5.is-size-5 Maijs Garais, 2024
 </template>
 
 <script>
@@ -58,93 +69,9 @@ $text-color: rgba(255, 255, 255, 0.75);
   flex: 1;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   text-align: center;
   box-shadow: inset 0 0 5rem rgba(0, 0, 0, 0.5);
-
-  .container {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    padding-top: 16px;
-    padding-bottom: 16px;
-    text-shadow: 0 0.05rem 0.1rem rgba(0, 0, 0, 0.5);
-
-    .action-btn {
-      font-weight: 700;
-      transition: transform 0.4s;
-      text-shadow: none;
-      margin-top: 1rem;
-      color: black;
-      background-color: white;
-      padding: 1rem;
-      border-radius: 5px;
-      margin-top: 1rem;
-      text-decoration: none;
-      display: inline-block;
-
-      &:hover {
-        transform: translate(0, -5px);
-      }
-    }
-
-    header {
-      margin-bottom: auto;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      width: 80%;
-
-      .title {
-        font-size: 1.2rem;
-        font-weight: 700;
-        color: $text-color;
-      }
-
-      nav {
-        a,
-        button {
-          color: rgba(255, 255, 255, 0.5);
-          padding: 0.25rem;
-          text-decoration: none;
-          transition: border-bottom-color 0.4s;
-          border-bottom: 0.15rem solid transparent;
-          border-radius: 0;
-          margin-left: 1rem;
-          font-weight: 700;
-
-          &:hover {
-            border-bottom-color: rgba(255, 255, 255, 0.5);
-          }
-
-          &.active {
-            color: white;
-            border-bottom-color: white;
-          }
-
-          &:focus {
-            background-color: rgba(255, 255, 255, 0.1);
-          }
-        }
-      }
-    }
-
-    footer {
-      margin-top: auto;
-      color: $text-color;
-    }
-
-    p {
-      margin-bottom: 0.7rem;
-      color: $text-color;
-    }
-
-    h1 {
-      margin-bottom: 1rem;
-    }
-  }
 }
 </style>
