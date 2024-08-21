ea1eca763426e740a3a97cec5966f57ded4059ab
Maijs Garais <mai-soup@mailfence.com>
2024-08-21 20:01:43 +0100

    separate navbar into its own component

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+header
+  nav.navbar(role='navigation' aria-label='main navigation')
+    .navbar-brand
+      RouterLink.navbar-item.has-text-weight-bold.is-size-5(to='/') Porch Reads Club
+      a.navbar-burger(role='button' aria-label='menu' :aria-expanded="isBurgerExpanded" :class='{"is-active": isBurgerExpanded}' @click='toggleBurger')
+        span(aria-hidden='true')
+        span(aria-hidden='true')
+        span(aria-hidden='true')
+        span(aria-hidden='true')
+    .navbar-menu(:class='{"is-active": isBurgerExpanded}')
+      .navbar-start
+        RouterLink.navbar-item.is-tab.is-active(to='/') Home
+        RouterLink.navbar-item.is-tab(to='/libraries') Libraries
+        RouterLink.navbar-item.is-tab(v-if='isLoggedIn' to='/books') Books
+      .navbar-end
+        RouterLink.navbar-item.is-tab(v-if='!isLoggedIn' to='/login') Login
+        RouterLink.navbar-item.is-tab(v-if='!isLoggedIn' to='/signup') Sign Up
+        a.navbar-item.is-tab(v-if='isLoggedIn' @click='doLogout') Logout
+</template>
+
+<script>
+import { RouterLink } from 'vue-router'
+import { useAccountStore } from '../stores/account'
+import { mapActions, mapState } from 'pinia'
+
+export default {
+  name: 'Navbar',
+  components: {
+    RouterLink
+  },
+  data() {
+    return {
+      isBurgerExpanded: false
+    }
+  },
+  computed: {
+    ...mapState(useAccountStore, ['isLoggedIn'])
+  },
+  methods: {
+    ...mapActions(useAccountStore, ['logout']),
+    async doLogout(e) {
+      console.log('doLogout')
+      e.preventDefault()
+      await this.logout()
+    },
+    toggleBurger() {
+      this.isBurgerExpanded = !this.isBurgerExpanded
+    }
+  }
+}
+</script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,22 +1,5 @@
 <template lang="pug">
-header
-  nav.navbar(role='navigation' aria-label='main navigation')
-    .navbar-brand
-      RouterLink.navbar-item.has-text-weight-bold.is-size-5(to='/') Porch Reads Club
-      a.navbar-burger(role='button' aria-label='menu' :aria-expanded="isBurgerExpanded" :class='{"is-active": isBurgerExpanded}' @click='toggleBurger')
-        span(aria-hidden='true')
-        span(aria-hidden='true')
-        span(aria-hidden='true')
-        span(aria-hidden='true')
-    .navbar-menu(:class='{"is-active": isBurgerExpanded}')
-      .navbar-start
-        RouterLink.navbar-item.active(to='/') Home
-        RouterLink.navbar-item(to='/libraries') Libraries
-        RouterLink.navbar-item(v-if='isLoggedIn' to='/books') Books
-      .navbar-end
-        RouterLink.navbar-item(v-if='!isLoggedIn' to='/login') Login
-        RouterLink.navbar-item(v-if='!isLoggedIn' to='/signup') Sign Up
-        a.navbar-item(v-if='isLoggedIn' @click='doLogout') Logout
+Navbar
 .bg
   div
   .hero
@@ -33,33 +16,12 @@ header
 </template>
 
 <script>
-import { RouterLink } from 'vue-router'
-import { useAccountStore } from '../stores/account'
-import { mapActions, mapState } from 'pinia'
+import Navbar from '@/components/Navbar.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    RouterLink
-  },
-  data() {
-    return {
-      isBurgerExpanded: false
-    }
-  },
-  computed: {
-    ...mapState(useAccountStore, ['isLoggedIn'])
-  },
-  methods: {
-    ...mapActions(useAccountStore, ['logout']),
-    async doLogout(e) {
-      console.log('doLogout')
-      e.preventDefault()
-      await this.logout()
-    },
-    toggleBurger() {
-      this.isBurgerExpanded = !this.isBurgerExpanded
-    }
+    Navbar
   }
 }
 </script>
