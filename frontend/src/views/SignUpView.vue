7f55c110d3f6e627044f654216ba1377b14a01b2
Maijs Garais <mai-soup@mailfence.com>
2024-08-22 19:14:08 +0100

    rewrtie signup view to use bulma, fix indentation in login view

--- frontend/src/views/SignUpView.vue
+++ frontend/src/views/SignUpView.vue
@@ -1,21 +1,26 @@
 <template lang="pug">
 .container
-  h1 Sign Up
+  h1.title Sign Up
   // TODO: prevent form submission altogether if validation fails, not just through button
   form(@submit.prevent="performSignUp")
-    div.form-group
-      label(for="username") Username
-      input.form-control(type="text" id="username" v-model="username" minlength="3" maxlength="24" pattern="^[a-zA-Z0-9_-]+$" required)
-      small(v-if="usernameError") {{ usernameError }}
-    div.form-group
-      label(for="email") Email
-      input.form-control(type="email" id="email" v-model="email" required)
-      small(v-if="emailError") {{ emailError }}
-    div.form-group
-      label(for="password") Password
-      input.form-control(type="password" id="password" v-model="password" required)
-    div.form-group
-      button.btn.btn-primary(type="submit" :disabled="shouldDisableSubmit") Sign Up
+    .field
+      .control.has-icons-left 
+        input.input(type="text" id="username" v-model="username" minlength="3" maxlength="24" pattern="^[a-zA-Z0-9_-]+$" placeholder="Username" required)
+        span.icon.is-small.is-left
+          font-awesome-icon(icon="user")
+    .field
+      .control.has-icons-left 
+        input.input(type="email" id="email" v-model="email" placeholder="Email" required)
+        span.icon.is-small.is-left
+          font-awesome-icon(icon="envelope")
+    .field
+      .control.has-icons-left 
+        input.input(type="password" id="password" v-model="password" placeholder="Password" required)
+        span.icon.is-small.is-left
+          font-awesome-icon(icon="lock")
+    .field
+      .control
+        button.button.is-success(type="submit" :disabled="shouldDisableSubmit") Sign Up
 </template>
 
 <script>
@@ -37,8 +42,15 @@ export default {
   },
   computed: {
     shouldDisableSubmit() {
-      return this.usernameError || this.emailError || this.passwordError || !this.username || !this.email || !this.password
-  }
+      return (
+        this.usernameError ||
+        this.emailError ||
+        this.passwordError ||
+        !this.username ||
+        !this.email ||
+        !this.password
+      )
+    }
   },
   methods: {
     ...mapActions(useAccountStore, ['signUp']),
@@ -55,21 +67,22 @@ export default {
         this.usernameError = 'Username must be at least 3 characters'
         return
       }
-      
+
       if (username.length > 24) {
         this.usernameError = 'Username cannot exceed 24 characters'
         return
-      } 
-      
+      }
+
       if (!/^[a-zA-Z0-9_-]+$/.test(username)) {
-        this.usernameError = 'Username can only contain letters, numbers, underscores and dashes'
+        this.usernameError =
+          'Username can only contain letters, numbers, underscores and dashes'
         return
       }
 
       this.usernameError = null
     },
     validateEmail(email) {
-      if(!validator.validate(email)) {
+      if (!validator.validate(email)) {
         this.emailError = 'Please enter a valid email address'
         return
       }
