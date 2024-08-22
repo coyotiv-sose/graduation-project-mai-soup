bbb02dbdae5b28e5f443a6f657725950d6af5217
Maijs Garais <mai-soup@mailfence.com>
2024-08-22 16:36:52 +0100

    fix home tab always being highlighted in navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -10,11 +10,11 @@ header
         span(aria-hidden='true')
     .navbar-menu(:class='{"is-active": isBurgerExpanded}')
       .navbar-start
-        RouterLink.navbar-item.is-tab.is-active(to='/') Home
+        RouterLink.navbar-item.is-tab(to='/') Home
         RouterLink.navbar-item.is-tab(to='/libraries') Libraries
         RouterLink.navbar-item.is-tab(v-if='isLoggedIn' to='/books') Books
       .navbar-end
-      //- TODO: add greeting if user logged in
+        //- TODO: add greeting if user logged in
         RouterLink.navbar-item.is-tab(v-if='!isLoggedIn' to='/login') Login
         RouterLink.navbar-item.is-tab(v-if='!isLoggedIn' to='/signup') Sign Up
         a.navbar-item.is-tab(v-if='isLoggedIn' @click='doLogout') Logout
