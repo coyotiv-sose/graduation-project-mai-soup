4afa1bf4e1bf0462638bd0da9e7550471d5012c6
Maijs Garais <mai-soup@mailfence.com>
2024-08-28 14:41:33 +0100

    extract memberships panel into its own component

---
+++ frontend/src/components/MembershipsPanel.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+.panel.is-info
+  p.panel-heading Memberships
+  a.panel-block(v-if="memberships.length === 0") {{ emptyMessage }}
+  RouterLink.panel-block(v-else v-for='library in memberships' :to="'/libraries/' + library._id" :key='library._id' :class="{ 'is-active': this.isOwnerOfLibrary(library._id) }")
+    span.panel-icon
+      font-awesome-icon(icon="building-columns" aria-hidden="true" :title="this.isOwnerOfLibrary(library._id) ? 'Owner' : 'Member'")
+    | {{ library.name }}
+</template>
+
+<script>
+export default {
+  name: 'MembershipsPanel',
+  props: {
+    memberships: {
+      type: Array,
+      required: true
+    },
+    ownedLibraries: {
+      type: Array,
+      required: true
+    },
+    emptyMessage: {
+      type: String,
+      default: 'This user is not a member of any libraries.'
+    }
+  },
+  methods: {
+    isOwnerOfLibrary(libraryId) {
+      return this.ownedLibraries.some((library) => library._id === libraryId)
+    }
+  }
+}
+</script>
--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -3,24 +3,20 @@
   div(v-if="!user") Loading...
   div(v-else)
     h1.title {{ user.username }}
-    .panel.is-info
-      p.panel-heading Memberships
-      a.panel-block(v-if="user.memberships.length === 0") This user is not a member of any libraries.
-      RouterLink.panel-block(v-else v-for='library in user.memberships' :to="'/libraries/' + library._id" :key='library._id' :class="{ 'is-active': this.isOwnerOfLibrary(library._id) }")
-        span.panel-icon
-          font-awesome-icon(icon="building-columns" aria-hidden="true" :title="this.isOwnerOfLibrary(library._id) ? 'Owner' : 'Member'")
-        | {{ library.name }}
+    MembershipsPanel(:memberships='user.memberships' :ownedLibraries='user.ownedLibraries')
 </template>
 
 <script>
 import { RouterLink } from 'vue-router'
 import { useProfileHandler } from '../stores/profile-handler'
 import { mapActions } from 'pinia'
+import MembershipsPanel from '../components/MembershipsPanel.vue'
 
 export default {
   name: 'ProfileView',
   components: {
-    RouterLink
+    RouterLink,
+    MembershipsPanel
   },
   data() {
     return {
@@ -28,12 +24,7 @@ export default {
     }
   },
   methods: {
-    ...mapActions(useProfileHandler, ['fetchProfile']),
-    isOwnerOfLibrary(libraryId) {
-      console.log('looking for libraryId', libraryId)
-      console.log('user.ownedLibraries', this.user.ownedLibraries)
-      return this.user.ownedLibraries.some((lib) => lib._id === libraryId)
-    }
+    ...mapActions(useProfileHandler, ['fetchProfile'])
   },
   async mounted() {
     this.user = await this.fetchProfile(this.$route.params.username)
